f1dae461125f943c171b754936bc6c49c51331a9
Jayson Reis <santosdosreis@gmail.com>
2013-02-27 13:02:14 -0300

    fixed flask patch

--- jac/frameworks/flask.py
+++ jac/frameworks/flask.py
@@ -1,4 +1,4 @@
 def patch_app(app):
     app.jinja_env.add_extension('jac.CompilerExtension')
-    app.jina_env.compressor_output_dir = app.config.COMPRESSOR_OUTPUT_DIR
-    app.jinja_env.compressor_static_prefix = app.config.COMPRESSOR_STATIC_PREFIX
+    app.jina_env.compressor_output_dir = app.config['COMPRESSOR_OUTPUT_DIR']
+    app.jinja_env.compressor_static_prefix = app.config['COMPRESSOR_STATIC_PREFIX']
