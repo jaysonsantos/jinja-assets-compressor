9397a1135fc935c6cd3a6fa9b0663998492fcd7f
Jayson Reis <santosdosreis@gmail.com>
2013-02-27 18:08:15 -0300

    bumpped version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='jac',
     author='Jayson Reis',
     author_email='santosdosreis@gmail.com',
-    version='0.2',
+    version='0.3',
     packages=find_packages(exclude=('tests*', )),
     install_requires=[
         'beautifulsoup4',
