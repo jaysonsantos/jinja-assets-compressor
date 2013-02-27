ee778b2d430016015ca50f8a9b64f0c68b7fb2cc
Jayson Reis <santosdosreis@gmail.com>
2013-02-27 12:48:21 -0300

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='jac',
+    author='Jayson Reis',
+    author_email='santosdosreis@gmail.com',
     version='0.1',
     packages=find_packages(exclude=('tests*', )),
-    install_requires=open('requirements.txt').read().split('\n')
+    install_requires=open('requirements.txt').read().split('\n'),
+    description='A Jinja extension (compatible with Flask and other frameworks) to compile and/or compress your assets.'
 )
