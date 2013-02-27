88e703b2761153109c24341655dba0a8ec240884
Jayson Reis <santosdosreis@gmail.com>
2013-02-27 16:39:05 -0300

    added a static finder to flask

--- jac/frameworks/flask.py
+++ jac/frameworks/flask.py
@@ -1,4 +1,24 @@
+def static_finder(app):
+    def find(path):
+        import os
+
+        for rule in app.url_map.iter_rules():
+            data = rule.match('|%s' % path)
+            if data:
+                (blueprint, view) = rule.endpoint.split('.')
+                blueprint = app.blueprints[blueprint]
+
+                filename = os.path.join(blueprint.static_folder, data['filename'])
+
+                return filename
+
+        raise IOError(2, 'File not found %s.' % path)
+
+    return find
+
+
 def patch_app(app):
     app.jinja_env.add_extension('jac.CompilerExtension')
     app.jinja_env.compressor_output_dir = app.config['COMPRESSOR_OUTPUT_DIR']
     app.jinja_env.compressor_static_prefix = app.config['COMPRESSOR_STATIC_PREFIX']
+    app.jinja_env.compressor_source_dirs = static_finder(app)
