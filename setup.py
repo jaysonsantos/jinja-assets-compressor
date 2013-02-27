83be9add132e9f9a25285d83fb4abe0aa844bde8
Jayson Reis <santosdosreis@gmail.com>
2013-02-27 18:40:50 -0300

    added more informations on setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,16 @@ setup(
     name='jac',
     author='Jayson Reis',
     author_email='santosdosreis@gmail.com',
-    version='0.4',
+    version='0.5',
     packages=find_packages(exclude=('tests*', )),
     install_requires=[
         'beautifulsoup4',
     ],
-    description='A Jinja extension (compatible with Flask and other frameworks) to compile and/or compress your assets.'
+    description='A Jinja extension (compatible with Flask and other frameworks) to compile and/or compress your assets.',
+    url='https://github.com/jaysonsantos/jinja-assets-compressor',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
 )
