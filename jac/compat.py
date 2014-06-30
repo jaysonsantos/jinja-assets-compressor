17b613b80f944b173f9113dd5a77873114510b0a
Alan Hamlett <alan.hamlett@gmail.com>
2014-06-30 16:21:17 -0700

    fix python2 unicode function

--- jac/compat.py
+++ jac/compat.py
@@ -10,9 +10,9 @@ is_py3 = (sys.version_info[0] == 3)
 if is_py2:
 
     def _u(text):
-        if type(text) != unicode:
+        if type(text) == str:
             return text.decode('utf-8')
-        return text
+        return unicode(text)
     u = _u
     open = codecs.open
 
