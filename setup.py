2963e02c63f6557572171c974255a6e82a158300
Alan Hamlett <alanhamlett@users.noreply.github.com>
2017-11-16 06:03:12 -0800

    use rst for pypi (#57)
    * use rst for readme because pypi does not render markdown
    
    * python syntax highlighting for code blocks

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires=open('requirements.txt').readlines(),
     description='A Jinja extension (compatible with Flask and other frameworks) '
                 'to compile and/or compress your assets.',
-    long_description=read('README.md'),
+    long_description=read('README.rst'),
     url='https://github.com/jaysonsantos/jinja-assets-compressor',
     classifiers=[
         'Development Status :: 4 - Beta',
